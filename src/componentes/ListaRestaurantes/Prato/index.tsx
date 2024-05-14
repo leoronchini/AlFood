691653a353a4c847e5a1552b39c185809e775f10
Leonardo <leo.ronchini@hotmail.com>
2024-05-13 23:44:56 -0300

    creating backend requisition with pagination

--- src/componentes/ListaRestaurantes/Prato/index.tsx
+++ src/componentes/ListaRestaurantes/Prato/index.tsx
@@ -1,29 +1,31 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import IPrato from '../../../interfaces/IPrato';
-import estilos from './Prato.module.scss';
+import IRestaurante from '../../../interfaces/IRestaurante';
+import Prato from '../Prato';
+import estilos from './Restaurante.module.scss';
 
-interface PratoProps {
-  prato: IPrato
+interface RestauranteProps {
+  restaurante: IRestaurante
 }
 
-const Prato = ({ prato }: PratoProps) => {
-  return (<div className={estilos.Prato}>
-    <div className={estilos.Container}>
-      <div>
-        <div className={estilos.EfeitoTorcao}>
-          <img src={prato.imagem} alt={prato.descricao}/>
-        </div>
-      </div>
+const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>()
+  useEffect(() => {
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+  }, [restaurante.id])
+
+  return (<section className={estilos.Restaurante}>
+    <div className={estilos.Titulo}>
+      <h2>{restaurante.nome}</h2>
     </div>
-    <div className={estilos.Conteudo}>
-      <h3>{prato.nome}</h3>
-      <div className={estilos.Tag}>
-        {prato.tag}
-      </div>
-      <div>
-        {prato.descricao}
-      </div>
+    <div>
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
-  </div>)
+  </section>)
 }
 
-export default Prato
+export default Restaurante
